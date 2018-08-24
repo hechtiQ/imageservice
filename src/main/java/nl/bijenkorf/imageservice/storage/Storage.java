bd0c36a14da31e9e21f23078b7769d2ab52c7b8b
hechtiq <yoram.spreij@gmail.com>
2018-08-24 11:39:50 +0200

    Added Storage interface to abstract implementations

---
+++ src/main/java/nl/bijenkorf/imageservice/storage/Storage.java
@@ -0,0 +1,13 @@
+package nl.bijenkorf.imageservice.storage;
+
+import nl.bijenkorf.imageservice.model.ImageType;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public interface Storage {
+
+    boolean hasImage(ImageType imageType, String filename);
+    void storeImage(ImageType imageType, String originalName, BufferedImage buffer) throws IOException;
+    BufferedImage getImage(ImageType imageType, String filename) throws IOException;
+}
