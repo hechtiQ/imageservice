f88fcd2175cea57485f69ae431502605b87efcbd
hechtiq <yoram.spreij@gmail.com>
2018-08-24 11:35:37 +0200

    Added PathHelper with createRelativePath method
    Method upholds the naming rules that were specified in the document
    - filename forward slashed need to be replaced with underscore
    - if filename is > 4 characters a subdir needs to be made with those
      4 characters
    - if filename is > 8 characters a second subdir needs to be made with
      those second set of 4 characters

---
+++ src/main/java/nl/bijenkorf/imageservice/util/PathHelper.java
@@ -0,0 +1,29 @@
+package nl.bijenkorf.imageservice.util;
+
+import nl.bijenkorf.imageservice.model.ImageType;
+
+import java.util.StringJoiner;
+
+public class PathHelper {
+
+    public static String createRelativePath(ImageType imageType, String filename) {
+        filename = filename.replaceAll("/", "_");
+
+        //Remove old file typing
+        filename = filename.substring(0, filename.lastIndexOf("."));
+
+        String suffix = "." + imageType.getTypeReturned().toString().toLowerCase();
+
+        StringJoiner stringJoiner = new StringJoiner("/", "", suffix);
+        stringJoiner.add(imageType.getPredefinedType().toString());
+        if (filename.length() > 4) {
+            stringJoiner.add(filename.substring(0,4));
+        }
+        if (filename.length() > 8) {
+            stringJoiner.add(filename.substring(4,8));
+        }
+        stringJoiner.add(filename);
+
+        return stringJoiner.toString();
+    }
+}
