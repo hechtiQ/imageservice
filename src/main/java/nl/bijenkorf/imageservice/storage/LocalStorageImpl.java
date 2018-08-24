5543032b003d4ce84899f78c426122c77713e891
hechtiq <yoram.spreij@gmail.com>
2018-08-24 11:40:41 +0200

    Added LocalStorageImpl with a quick initial implementation

---
+++ src/main/java/nl/bijenkorf/imageservice/storage/LocalStorageImpl.java
@@ -0,0 +1,43 @@
+package nl.bijenkorf.imageservice.storage;
+
+import nl.bijenkorf.imageservice.model.ImageType;
+import nl.bijenkorf.imageservice.util.PathHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class LocalStorageImpl implements Storage{
+
+    private Logger log = LoggerFactory.getLogger(LocalStorageImpl.class);
+    private final String storageFolder = "storage";
+
+    public LocalStorageImpl() {
+        File storageDir = new File(storageFolder);
+        if (!storageDir.mkdir()) {
+            log.error("Couldn't create local storage folder");
+        }
+    }
+
+
+    @Override
+    public boolean hasImage(ImageType imageType, String filename) {
+        File imageFile = new File(storageFolder + "/" + PathHelper.createRelativePath(imageType, filename));
+        return imageFile.exists();
+    }
+
+    @Override
+    public void storeImage(ImageType imageType, String originalName, BufferedImage buffer) throws IOException{
+        String fileType = imageType.getTypeReturned().toString().toLowerCase();
+
+        ImageIO.write(buffer, fileType, new File(storageFolder + "/" + PathHelper.createRelativePath(imageType, originalName)));
+    }
+
+    @Override
+    public BufferedImage getImage(ImageType imageType, String filename) throws IOException {
+        return ImageIO.read(new File(storageFolder + "/" + PathHelper.createRelativePath(imageType, filename)));
+    }
+}
