31828267c1d85388c26371e96a0fd2c85c61c4b8
hechtiq <yoram.spreij@gmail.com>
2018-08-24 13:17:08 +0200

    Added status codes to the exceptions in the hope those work

--- src/main/java/nl/bijenkorf/imageservice/exception/UnsupportedPredefinedTypeException.java
+++ src/main/java/nl/bijenkorf/imageservice/exception/UnsupportedPredefinedTypeException.java
@@ -1,5 +1,9 @@
 package nl.bijenkorf.imageservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UnsupportedPredefinedTypeException extends RuntimeException {
 
     public UnsupportedPredefinedTypeException() {
--- src/main/java/nl/bijenkorf/imageservice/exception/UnsupportedScaleTypeException.java
+++ src/main/java/nl/bijenkorf/imageservice/exception/UnsupportedScaleTypeException.java
@@ -1,5 +1,9 @@
 package nl.bijenkorf.imageservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UnsupportedScaleTypeException extends RuntimeException {
 
     public UnsupportedScaleTypeException() {
