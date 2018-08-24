5659e992f5721df0534b0f68126e41283381d540
hechtiq <yoram.spreij@gmail.com>
2018-08-24 11:32:21 +0200

    Changed ImageServiceController to @RestController annotation

--- src/main/java/nl/bijenkorf/imageservice/controller/ImageServiceController.java
+++ src/main/java/nl/bijenkorf/imageservice/controller/ImageServiceController.java
@@ -1,14 +1,10 @@
 package nl.bijenkorf.imageservice.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@Controller
+@RestController
 public class ImageServiceController {
 
 
